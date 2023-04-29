6c71883703eeff967d8cc8f334ba4a5ad44a6e21
Brandon Nguyen <brandonnguyen257@gmail.com>
2023-04-29 13:41:33 -0700

    removed unecessary comments

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
-# from setuptools import setup, find_packages
-
-# setup(
-#     name="HapticMetadataGeneration",
-#     version="0.1.0",
-#     author="Brandon Nguyen",
-#     author_email="brandonnguyen257@gmail.com",
-#     description="Code for generating metadata for haptic data",
-#     packages=find_packages(),
-#     install_requires=[
-#         "numpy",
-#         "pandas",
-#         "scikit-learn",
-#     ],
-# )
-
-
 from setuptools import setup, find_packages
 
+#we pull the same depdencies from requirements.txt into setup.py
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
